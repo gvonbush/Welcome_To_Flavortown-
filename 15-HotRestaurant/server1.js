8c38b3d326091b182ed278977335ebb621e551da
Hava-hizzle <mikehavrilak@gmail.com>
2020-01-04 11:54:26 -0500

    first commit

--- 15-HotRestaurant/server1.js
+++ 15-HotRestaurant/server1.js
@@ -9,3 +9,12 @@ app.use(express.json());
 app.listen(PORT, function() {
    console.log("App listening on PORT " + PORT);
 });
+
+
+app.post("/api/reservations", function(req, res) {
+   var newReservation = req.body;
+
+   newReservation.routeName = newReservation.name.place()
+
+   console.log(newReservation);
+})
