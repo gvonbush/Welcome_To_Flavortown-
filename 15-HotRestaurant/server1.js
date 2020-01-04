b39e69b595390a5cb6cb79085ceae27233eacd24
Robin Jones <RobinJones335@gmail.com>
2020-01-04 12:14:50 -0500

    get post

--- 15-HotRestaurant/server1.js
+++ 15-HotRestaurant/server1.js
@@ -1,4 +1,41 @@
-var express = require("express");
+let express = require("express");
 
-var app = express();
-var PORT = 3000;
+let app = express();
+let PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+   console.log("App listening on PORT " + PORT);
+});
+let Reservations = [
+   {
+     routeName: "",
+     name: "",
+   },
+   {
+     routeName: "",
+     name: "",
+   },
+   {
+     routeName: "",
+     name: "",
+     
+   }
+ ];
+app.post("/api/Reservations", function(req, res) {
+   // req.body hosts is equal to the JSON post sent from the user
+   // This works because of our body parsing middleware
+   var newReservations = req.body;
+ 
+   // Using a RegEx Pattern to remove spaces from newCharacter
+   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+   newReservations.routeName = newReservation.name.place(/\s+/g, "").toLowerCase();
+ 
+   console.log(newReservations);
+ 
+   characters.push(newReservations);
+ 
+   res.json(newReservations);
+ });
